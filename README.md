346c352aced4a2bf68e64638ac4d76948d031080
mindziukasss <mindziukasss@gmail.com>
2017-01-05 23:35:15 +0200

    Add css and javascript

---
+++ README.md
@@ -0,0 +1,4 @@
+# README
+
+This simple site is a static with html page.
+FreeCodeCampe task.
---
+++ javascripts/application.js
@@ -0,0 +1,20 @@
+// Floating label headings for the contact form
+// $(function() {$("body")
+
+$(function() {   
+  $('body').on('click', 'a.scrollable', function(event) {
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({target: '.navbar-fixed-top'})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $(".navbar-collapse").collapse('hide');});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {$('body').on('click', 'a.scrollable', function(event) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+  event.preventDefault();
+  });
+});
+
